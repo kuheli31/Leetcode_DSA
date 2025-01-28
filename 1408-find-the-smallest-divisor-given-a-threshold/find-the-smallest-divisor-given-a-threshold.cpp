f8f6135b1b49459846276b0ac888e50aed422e3f
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-01-29 00:39:24 +0530

    Time: 10 ms (57.32%) | Memory: 26.1 MB (81.23%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -13,13 +13,11 @@ public:
     int smallestDivisor(vector<int>& nums, int threshold) 
     {
         long long low=1 , high = *max_element(nums.begin(), nums.end());;
-        long long result=high;
         while(low <= high)
         {
             long long mid= low + (high - low)/2;
             if(division(nums , mid) <= threshold)
             {
-                result = mid;
                 high = mid -1;
             }
             else
@@ -28,6 +26,6 @@ public:
             }
         }
         
-        return result;
+        return low;
     }
 };
