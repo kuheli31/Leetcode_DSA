756a8aeeb2b90796dc3181327eb0bcef146af221
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-02-05 13:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (53.04%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,31 +1,44 @@
 class Solution {
-private:
-    int solve(vector<int>& arr, int mid) {
-        int cnt = 1; 
-        long long sum = 0;
-        for (int i : arr) {
-            if (sum + i <= mid) {
-                sum += (long long)i;
-            } else {
-                cnt++; 
-                sum = (long long)i;
+public:
+    int countArr(vector<int>& nums, int sumArr)//pass no. of subarrays
+    {
+        int n=nums.size();
+        int subArr=1;
+        long long countSum=0;
+        for(int i=0 ; i<n ; i++)
+        {
+            if(nums[i] + countSum <= sumArr)
+            {
+                countSum += nums[i];
+
+            }
+            else
+            {
+                subArr++;
+                countSum = nums[i];
             }
         }
-        return cnt;
+        return subArr;
     }
 
-public:
-    int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(), nums.end()); 
-        int high = accumulate(nums.begin(), nums.end(), 0);
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2; 
-            int cnt = solve(nums, mid);
-            if (cnt > k) {
-                low = mid + 1; 
-            } else {
-                high = mid - 1;
+    int splitArray(vector<int>& nums, int k) 
+    {
+        int n=nums.size();
+        int low = *max_element(nums.begin() , nums.end());
+        int high =accumulate(nums.begin() , nums.end() , 0);
+
+        if(k>n) return -1;
+
+        while(low<=high)
+        {
+            int mid = low + (high - low)/2;
+            if(countArr(nums , mid) > k)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
             }
         }
         return low;
