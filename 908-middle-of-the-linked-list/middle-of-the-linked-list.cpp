8e8df62fda7894b067b400c31b7c2ac9736fafdf
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-02-11 20:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (63.03%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -12,49 +12,13 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) 
     {
-        ListNode *p = head;
-        int count=0;
-        //Traversing to find number of nodes
-        while(p!=NULL)
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast!=0 && fast->next!=0)
         {
-            count++;
-            p=p->next;
-        }
-        //to reuse pointer p
-        p = head;
-        //Checking if count is even or odd
-        if(count%2 == 0)
-        {
-            int midEven=(count + 1)/2;
-            for(int i=0 ; i<midEven ; i++)
-            {
-                p=p->next;
-            }
-            //Printing nodes after middle node
-            ListNode *list = p;
-            int q;
-            while(list!=0)
-            {
-                q=list->val;
-                list = list->next;
-            }
-        }
-        else
-        {
-            int midOdd=(count + 2)/2;
-            for(int i=0 ; i<midOdd-1 ; i++)
-            {
-                p=p->next;
-            }
-            //Printing nodes after middle node
-            ListNode *list = p;
-            int q;
-            while(list!=0)
-            {
-                q=list->val;
-                list = list->next;
-            }
-        }
-        return p;
+            slow=slow->next;
+            fast=fast->next->next;
+        } 
+        return slow;
     }
 };
