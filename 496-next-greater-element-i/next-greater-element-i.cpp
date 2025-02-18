a307d3b420272a84ba9ef2bdb8957aae2c62b3da
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-02-18 19:56:17 +0530

    Time: 3 ms (45.21%) | Memory: 12.9 MB (11.75%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> ngeMap;
-        stack<int> st;
+        unordered_map<int, int> ngeMap;  // Map to store the next greater element for each number in nums2
+        stack<int> st;  // Monotonic decreasing stack to track the next greater element
 
-        for(int i=nums2.size()-1 ; i>=0 ;  i--)
-        {
-            while(!st.empty() && st.top()<=nums2[i])
-            {
-                st.pop();
-            }
-            
-            if (st.empty()) 
-            {
-                ngeMap[nums2[i]] = -1;
-            } 
-            else 
-            {
-                ngeMap[nums2[i]] = st.top();
+        // Iterate over nums2 from right to left (reverse order)
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            // Pop elements from the stack that are smaller than or equal to nums2[i]
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();  // Remove smaller elements, they cannot be the NGE
             }
 
+            // If stack is empty, there is no greater element to the right
+            if (st.empty()) {
+                ngeMap[nums2[i]] = -1;  // No NGE found, assign -1
+            } else {
+                ngeMap[nums2[i]] = st.top();  // Assign the top of the stack as NGE
+            }
+
+            // Push the current element onto the stack for future comparisons
             st.push(nums2[i]);
         }
 
+        // Prepare the result for nums1 by looking up the values in ngeMap
         vector<int> result(nums1.size());
-        for(int i=0 ; i<nums1.size() ; i++)
-        {
-            result[i]= ngeMap[nums1[i]];
+        for (int i = 0; i < nums1.size(); i++) {
+            result[i] = ngeMap[nums1[i]];  // Retrieve precomputed NGE from the map
         }
-        return result;
+
+        return result;  // Return the final result
     }
 };
