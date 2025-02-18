fa828dd4fdb99672fdec0338dba9dbdf00d310d8
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-02-18 13:46:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (52.17%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,23 +4,28 @@ public:
     {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        unordered_map<int, int> mpp; // Stores index of each element in nums2
-        vector<int> result(n1, -1);  // Initialize result with -1
+        unordered_map<int, int> ngeMap; // Stores the next greater element for nums2
+        stack<int> st; // Monotonic decreasing stack
 
-        // Step 1: Store the index of each element in nums2
-        for (int j = 0; j < n2; j++) {
-            mpp[nums2[j]] = j;
+        // Step 1: Find Next Greater Elements for nums2 using Stack
+        for (int i = 0; i < n2; i++) {
+            while (!st.empty() && st.top() < nums2[i]) {
+                ngeMap[st.top()] = nums2[i]; // Map the next greater element
+                st.pop();
+            }
+            st.push(nums2[i]);
         }
 
-        // Step 2: Find next greater element for nums1[i] in nums2
+        // Remaining elements in stack have no NGE, mark them as -1
+        while (!st.empty()) {
+            ngeMap[st.top()] = -1;
+            st.pop();
+        }
+
+        // Step 2: Populate result for nums1 using the map
+        vector<int> result(n1);
         for (int i = 0; i < n1; i++) {
-            int index = mpp[nums1[i]]; // Get the index of nums1[i] in nums2
-            for (int j = index + 1; j < n2; j++) { // Search for next greater
-                if (nums2[j] > nums1[i]) {
-                    result[i] = nums2[j];
-                    break;
-                }
-            }
+            result[i] = ngeMap[nums1[i]];
         }
 
         return result;
