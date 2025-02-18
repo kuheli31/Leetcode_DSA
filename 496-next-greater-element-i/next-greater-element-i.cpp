54792fe7eadefaa5b4a2e4ca0d83db11f55a4b30
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-02-18 19:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (30.53%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
-    {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        unordered_map<int, int> ngeMap; // Stores the next greater element for nums2
-        stack<int> st; // Monotonic decreasing stack
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> ngeMap;
+        stack<int> st;
 
-        // Step 1: Find Next Greater Elements for nums2 using Stack
-        for (int i = 0; i < n2; i++) {
-            while (!st.empty() && st.top() < nums2[i]) {
-                ngeMap[st.top()] = nums2[i]; // Map the next greater element
+        for(int i=nums2.size()-1 ; i>=0 ;  i--)
+        {
+            while(!st.empty() && st.top()<=nums2[i])
+            {
                 st.pop();
             }
+            
+            if (st.empty()) 
+            {
+                ngeMap[nums2[i]] = -1;
+            } 
+            else 
+            {
+                ngeMap[nums2[i]] = st.top();
+            }
+
             st.push(nums2[i]);
         }
 
-        // Remaining elements in stack have no NGE, mark them as -1
-        while (!st.empty()) {
-            ngeMap[st.top()] = -1;
-            st.pop();
+        vector<int> result(nums1.size());
+        for(int i=0 ; i<nums1.size() ; i++)
+        {
+            result[i]= ngeMap[nums1[i]];
         }
-
-        // Step 2: Populate result for nums1 using the map
-        vector<int> result(n1);
-        for (int i = 0; i < n1; i++) {
-            result[i] = ngeMap[nums1[i]];
-        }
-
         return result;
     }
 };
