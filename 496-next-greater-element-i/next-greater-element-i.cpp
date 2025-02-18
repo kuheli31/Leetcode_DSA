c907a6236bce590c173c09e33c9b458cf654eb21
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-02-18 13:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (52.17%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,33 +2,27 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
     {
-        int n1=nums1.size();
-        int n2=nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        unordered_map<int, int> mpp; // Stores index of each element in nums2
+        vector<int> result(n1, -1);  // Initialize result with -1
 
-        vector<int> result(n1);
-        for(int i=0 ; i<n1 ; i++)
-        {
-            bool found=false;// to find element of nums1 in nums2
-            for(int j=0 ; j<n2 ; j++)
-            {
-                if(nums1[i] == nums2[j])
-                {
-                    found=true;
-                }
-                if(found && nums2[j]>nums1[i])
-                {
-                    result[i]=nums2[j];
+        // Step 1: Store the index of each element in nums2
+        for (int j = 0; j < n2; j++) {
+            mpp[nums2[j]] = j;
+        }
+
+        // Step 2: Find next greater element for nums1[i] in nums2
+        for (int i = 0; i < n1; i++) {
+            int index = mpp[nums1[i]]; // Get the index of nums1[i] in nums2
+            for (int j = index + 1; j < n2; j++) { // Search for next greater
+                if (nums2[j] > nums1[i]) {
+                    result[i] = nums2[j];
                     break;
                 }
             }
         }
-        for(int i=0 ; i<result.size() ; i++)
-        {
-            if(result[i] == 0)
-            {
-                result[i]=-1;
-            }
-        }
+
         return result;
     }
 };
