9a90cb9748a23a640387f849b46b02f4e9211dec
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-02-25 22:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (99.07%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -3,7 +3,7 @@ public:
     char kthCharacter(int k) {
         if (k == 1) return 'a';
         int len = 1;
-        while (len < k) len *= 2;
+        while (len < k) len = len*2;
         int half = len / 2;
         if (k <= half) {
             return kthCharacter(k);
