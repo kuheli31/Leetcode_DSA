5858fefc6a46bc44e6382d9b83dc3cbd8dcff6d2
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-03-07 22:50:17 +0530

    Time: 6 ms (14.01%) | Memory: 14.3 MB (19.27%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,36 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    void backtrack(int index, vector<int>& nums, vector<int>& temp, set<vector<int>>& result) {
-        // Insert the current subset into the set (ensures uniqueness)
-        result.insert(temp);
+    set<vector<int>> resultSet;  // Stores unique subsets
 
-        // Explore further elements
-        for (int i = index; i < nums.size(); i++) {
-            // Pick the element
-            temp.push_back(nums[i]);
+    void backtrack(int index, vector<int>& nums, vector<int>& temp) 
+    {
+        resultSet.insert(temp);
+        if (index >= nums.size()) return;
 
-            // Recur for the next index
-            backtrack(i + 1, nums, temp, result);
+        temp.push_back(nums[index]);
+        backtrack(index + 1, nums, temp);
+        temp.pop_back(); // Backtrack step
 
-            // Backtrack - remove the last added element
-            temp.pop_back();
-        }
+        // Skip duplicate elements by advancing the index
+        while (index + 1 < nums.size() && nums[index] == nums[index + 1]) index++;
+        backtrack(index + 1, nums, temp);
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> resultSet;  // Set to store unique subsets
+        sort(nums.begin(), nums.end()); 
         vector<int> temp;
-
-        // Step 1: Sort the array to handle duplicates
-        sort(nums.begin(), nums.end());
-
-        // Step 2: Start backtracking from index 0
-        backtrack(0, nums, temp, resultSet);
-
-        // Convert set to vector of vectors
-        return vector<vector<int>>(resultSet.begin(), resultSet.end());
+        backtrack(0, nums, temp);
+        return vector<vector<int>>(resultSet.begin(), resultSet.end()); // Convert set to vector
     }
 };
