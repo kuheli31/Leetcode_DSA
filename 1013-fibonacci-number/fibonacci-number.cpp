22e9db22d5c3f52bcc5d69ec8c7622ceedac1e34
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-07-29 21:14:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
+     vector<int> dp = vector<int>(31, -1);
     int fib(int n) 
     {
         if(n<=1)
         {
             return n;
         }
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
         else
         {
-            return fib(n-2)+fib(n-1);
+            return dp[n] = fib(n-2)+fib(n-1);
         }
     }
 };
