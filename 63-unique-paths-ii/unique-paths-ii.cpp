055e9000c8762de7473975596df38f0fd74a9f45
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-08-26 21:13:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (86.98%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int solve(int i, int j , vector<vector<int>>& obstacleGrid , vector<vector<int>> &dp)
-    {
-        // Out of bounds
-        if(i < 0 || j < 0) return 0;
-
-        // Obstacle
-        if(obstacleGrid[i][j] == 1) return 0;
-
-        // Reached start
-        if(i == 0 && j == 0) return 1;
-
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-
-        int up = solve(i-1 , j , obstacleGrid , dp);
-        int left = solve(i , j-1 , obstacleGrid , dp);
-
-        return dp[i][j] = up + left;
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        vector<vector<int>> dp(m ,vector<int>(n , -1));
-        return solve(m-1 , n-1 , obstacleGrid , dp);
+        vector<int> dp(n, 0);
+
+        for(int i = 0; i < m; i++) {
+            vector<int> temp(n, 0); // current row
+            for(int j = 0; j < n; j++) {
+                if(obstacleGrid[i][j] == 1) {
+                    temp[j] = 0; // obstacle → no path
+                }
+                else if(i == 0 && j == 0) {
+                    temp[j] = 1; // starting point
+                }
+                else {
+                    int up = (i > 0) ? dp[j] : 0;
+                    int left = (j > 0) ? temp[j-1] : 0;
+                    temp[j] = up + left;
+                }
+            }
+            dp = temp; // move to next row
+        }
+
+        return dp[n-1];
     }
 };
