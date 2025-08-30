6902db31dd0e2998d4aa9177728d238f63768cd3
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-08-30 13:54:35 +0530

    Time: 54 ms (45.04%) | Memory: 20.8 MB (71.70%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,55 +1,50 @@
 class Solution {
 public:
-    int solve(int i , int j1 , int j2 , vector<vector<int>>& grid , vector<vector<vector<int>>> &dp)
-    {
-        int r , c;
-        r = grid.size();
-        c = grid[0].size();
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
 
-        //Out of Bound
-        if(j1<0 || j2<0 || j1>=c || j2>=c)
-        {
-            return -1e9;
-        }
-        //Possible ans
-        if(i == r-1)
-        {
-            if(j1 == j2) return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
-        }
+        // 3D DP table initialized to -1
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
 
-        if(dp[i][j1][j2] != -1)
-        {
-            return dp[i][j1][j2];
-        }
-
-        //Explore all the paths
-        int maxi = -1e9;
-        for(int dj1=-1 ; dj1<=+1 ; dj1++)
-        {
-            for(int dj2=-1 ; dj2<=+1 ; dj2++)
-            {
-                int value=0;
-                if(j1 == j2) 
-                {
-                    value = grid[i][j1];
-                } 
-                else 
-                {
-                    value += grid[i][j1] + grid[i][j2] + solve(i+1 , j1+dj1 , j2+dj2 , grid , dp);
-                }
-                maxi = max(maxi , value);
+        // Base case: last row
+        for (int j1 = 0; j1 < n; j1++) {
+            for (int j2 = 0; j2 < n; j2++) {
+                if (j1 == j2) dp[m-1][j1][j2] = grid[m-1][j1];
+                else dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
             }
         }
-        return dp[i][j1][j2] = maxi;
-    }
 
-    int cherryPickup(vector<vector<int>>& grid) {
-        int m , n;
-        m = grid.size();
-        n = grid[0].size();
-        //dp[r][c][c]
-        vector<vector<vector<int>>> dp(m , vector<vector<int>>(n , vector<int>(n , -1)));
-        return solve(0, 0, n-1, grid , dp);
+        // Fill from bottom-1 row to top
+        for (int i = m-2; i >= 0; i--) {
+            for (int j1 = 0; j1 < n; j1++) {
+                for (int j2 = 0; j2 < n; j2++) {
+                    int maxi = -1e9;
+
+                    // Explore all moves
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            int value = 0;
+                            if (j1 == j2) value = grid[i][j1];
+                            else value = grid[i][j1] + grid[i][j2];
+
+                            // Next row positions
+                            int nj1 = j1 + dj1;
+                            int nj2 = j2 + dj2;
+                            if (nj1 >= 0 && nj1 < n && nj2 >= 0 && nj2 < n) {
+                                value += dp[i+1][nj1][nj2];
+                            } else {
+                                value += -1e9; // invalid move
+                            }
+                            maxi = max(maxi, value);
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+
+        // Answer from (0,0) and (0,n-1)
+        return dp[0][0][n-1];
     }
 };
