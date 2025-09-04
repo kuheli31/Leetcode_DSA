223e54caacdc1355f83f834faaf8c21cb92538dd
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2025-09-04 21:08:03 +0530

    Time: 15 ms (93.24%) | Memory: 17.8 MB (94.03%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -4,37 +4,19 @@ public:
     {
         int n = coins.size();
         const int INF = 1e9;
-        vector<int> prev(amount+1 , 0);
-        vector<int> curr(amount+1 , 0);
-        // Base case: using only coin[0]
-        for (int t = 0; t <= amount; t++) {
-            if (t % coins[0] == 0) prev[t] = t / coins[0];
-            else prev[t] = INF;
-        }
+        vector<int> dp(amount + 1, INF);
 
-        // Build DP table
-        for (int ind = 1; ind < n; ind++) {
-            for (int t = 0; t <= amount; t++) {
-                int notTake = prev[t];
-                int take = INF;
-                if (coins[ind] <= t) {
-                    take = 1 + curr[t - coins[ind]];
-                }
-                curr[t] = min(take, notTake);
+        // Base case
+        dp[0] = 0;
+
+        // For each coin, update dp[]
+        for (int ind = 0; ind < n; ind++) {
+            for (int t = coins[ind]; t <= amount; t++) {
+                dp[t] = min(dp[t], 1 + dp[t - coins[ind]]);
             }
-            prev = curr;
-            fill(curr.begin(), curr.end(), 0);
         }
 
-        int ans = prev[amount];
-
-        if(ans >= INF)
-        {
-            return -1;
-        }
-        else
-        {
-            return ans;
-        }
+        int ans = dp[amount];
+        return (ans >= INF) ? -1 : ans;
     }
 };
