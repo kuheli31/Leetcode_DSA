e6b6477491e2a1fea0e029f1569f6bef1116537a
Kuheli Bera <133209155+kuheli31@users.noreply.github.com>
2026-07-15 11:04:42 +0530

    Time: 71 ms (11.07%) | Memory: 14.1 MB (82.15%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-         // Step 1: Store the original indices
-        vector<pair<int, int>> indexed_nums;
-        for (int i = 0; i < nums.size(); i++) {
-            indexed_nums.push_back({nums[i], i});
-        }
-
-        // Step 2: Sort the array based on values
-        sort(indexed_nums.begin(), indexed_nums.end());
-
-        // Step 3: Initialize two pointers
-        int left = 0;
-        int right = indexed_nums.size() - 1;
-
-        // Step 4: Two-pointer logic
-        while (left < right) {
-            int sum = indexed_nums[left].first + indexed_nums[right].first;
-            if (sum == target) {
-                // Return the original indices
-                return {indexed_nums[left].second, indexed_nums[right].second};
-            } else if (sum < target) {
-                left++;  // Move left pointer to the right
-            } else {
-                right--; // Move right pointer to the left
-            }
-        }
-
-        return {}; // If no solution, return an empty vector
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> ans;
+	for(int i=0 ; i<nums.size() ; i++)
+	{
+		for(int j=i+1 ; j< nums.size() ; j++)
+		{
+			if((nums[i] + nums[j]) == target)
+			{
+				ans.push_back(i);
+				ans.push_back(j);
+				return ans;
+			}
+		}
+	}
+	return ans;	     
     }
 };
